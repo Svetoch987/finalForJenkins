88e2567c98b8ca4f1a7160925b2aa0da9c245838
Svetoch987 <clenet@mail.ru>
2017-06-16 12:21:20 +0300

    Update cart.spec.js

--- test/cart.spec.js
+++ test/cart.spec.js
@@ -1,24 +1,4 @@
-describe("creating cart", function() {
-    var $rootScope;
-    var $controller;
-    beforeEach(module("cart"));
-    beforeEach(inject(function($injector) {
 
-        $rootScope = $injector.get('$rootScope');
-        $controller = $injector.get('$controller');
-        $scope = $rootScope.$new();
-
-    }));
-    beforeEach(inject(function($controller) {
-        ctrl = $componentController('cartController');
-    }));
-
-    it('should create cart with 0 items in it', function() {
-      expect(ctrl.items_Cart.length).toBe(0);
-    });
-
-}); 
-/*
 describe('creating cart', function() {
 
   beforeEach(module('cart'));
