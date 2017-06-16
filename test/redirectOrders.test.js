b290e77efddfb6848d7b41e8b95a7c92787dbb8d
Svetoch987 <clenet@mail.ru>
2017-06-16 17:39:47 +0300

    Update redirectOrders.test.js

--- test/redirectOrders.test.js
+++ test/redirectOrders.test.js
@@ -1,1 +1,18 @@
+describe('storeApp', function() {
 
+  describe('storeMenuController', function() {
+
+    beforeEach(function() {
+      browser.get('http://192.168.7.60:5050/#!/');
+    });
+
+    it('should redirect to cataloge page', function() {
+
+      element.all(by.xpath('//*[@class="fa fa-money"]')).click();
+      
+      expect(browser.getCurrentUrl()).toBe('http://192.168.7.60:5050/#!/acerca');
+    });
+
+  });
+
+});
