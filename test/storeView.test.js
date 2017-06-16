feb9a2f720ad3692a916b106b57123ec639d2f2d
Svetoch987 <clenet@mail.ru>
2017-06-16 15:28:05 +0300

    Update storeView.test.js

--- test/storeView.test.js
+++ test/storeView.test.js
@@ -3,22 +3,16 @@ describe('storeApp', function() {
   describe('storeController', function() {
 
     beforeEach(function() {
-      browser.get('storeView.html');
+      browser.get('http://localhost:5050/#!/tienda');
     });
 
     it('should check the items in shop', function() {
-      //var itemList = element.all(by.repeater('product in query'));
+      var itemList = element.all(by.repeater('product in query'));
 
-      //expect(itemList.count()).toBe(7);
-      expect(true.toBe(true));
+      expect(itemList.count()).toBe(7);
+      //expect(true.toBe(true));
     });
 
   });
 
 });
-/*
-var products = ['Photoshop','Corel','Inkscape'];
-it('should create 3 products', function() {
-
-    expect(products.length).toBe(3);
- }); */
