d308854c8fa39e987b8cd2dfca6c461897efb87f
Svetoch987 <clenet@mail.ru>
2017-06-16 15:30:44 +0300

    Update storeView.test.js

--- test/storeView.test.js
+++ test/storeView.test.js
@@ -3,7 +3,7 @@ describe('storeApp', function() {
   describe('storeController', function() {
 
     beforeEach(function() {
-      browser.get('http://localhost:5050/#!/tienda');
+      browser.get('http://localhost:5050/#!/tienda', 5000);
     });
 
     it('should check the items in shop', function() {
