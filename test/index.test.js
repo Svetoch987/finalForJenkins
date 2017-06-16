6bca42f29b93ad7b0937639b66b1d948d121b821
Svetoch987 <clenet@mail.ru>
2017-06-16 17:04:04 +0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -6,13 +6,11 @@ describe('storeApp', function() {
       browser.get('http://192.168.7.60:5050/#!/');
     });
 
-    it('should render specific links', function() {
+    it('should redirect to cataloge page', function() {
 
       element.all(by.xpath('//*[@class="btn btn-info"]')).click();
       
       expect(browser.getCurrentUrl()).toBe('http://192.168.7.60:5050/#!/tienda');
-                                            
-      
     });
 
   });
