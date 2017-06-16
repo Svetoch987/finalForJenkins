90ad1af2756484bb9a1048cb9d3668c691649955
Svetoch987 <clenet@mail.ru>
2017-06-16 14:01:54 +0300

    Update cart.spec.js

--- test/cart.spec.js
+++ test/cart.spec.js
@@ -2,12 +2,12 @@
 describe('creating cart', function() {
 
   beforeEach(module('cart'));
-
+/*
   it('should create cart with 0 items in it', inject(function($controller) {
     var scope = {};
     var ctrl = $controller('cartController', {$scope: scope});
 
     expect(scope.items_Cart.length).toBe(0);
-  }));
+  }));*/
 
 });
