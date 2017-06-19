c2a7078a1cb1bffc68ec840407ed77e35dc154dd
Svetoch987 <clenet@mail.ru>
2017-06-19 13:55:01 +0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,22 @@
-grunt.initConfig({
-    docular: {
-        useHtml5Mode: false, //Use angular's html5 mode? true/false.
-        docular_webapp_target: '/docs', //The place where the docs will be generated
-        showAngularDocs: true,
-        showDocularDocs: true,
-        examples: {}, //instructions for how to run the sandboxed examples
-        groups: [] //groups of documentation to parse
-    }
-})
+    module.exports = function(grunt) {
+     
+        // Project configuration.
+        grunt.initConfig({
+     
+            pkg: grunt.file.readJSON('package.json'),
+     
+            docular: {
+                groups: [],
+                showDocularDocs: true,
+                showAngularDocs: true
+            }
+     
+        });
+     
+        // Load the plugin that provides the "docular" tasks.
+        grunt.loadNpmTasks('grunt-docular');
+     
+        // Default task(s).
+        grunt.registerTask('default', ['docular']);
+     
+    };
