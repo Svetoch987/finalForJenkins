4512e1f9c8ab9ed77619ae58bb6db05cb33f344e
Svetoch987 <clenet@mail.ru>
2017-06-19 13:49:26 +0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,10 @@
-var grunt = require('grunt');
-
 grunt.initConfig({
-    bump: {
-        options: {
-            pushTo: 'origin'
-        }
+    docular: {
+        useHtml5Mode: false, //Use angular's html5 mode? true/false.
+        docular_webapp_target: '/docs', //The place where the docs will be generated
+        showAngularDocs: true,
+        showDocularDocs: true,
+        examples: {}, //instructions for how to run the sandboxed examples
+        groups: [] //groups of documentation to parse
     }
 })
-
-grunt.loadNpmTasks('grunt-bump');
