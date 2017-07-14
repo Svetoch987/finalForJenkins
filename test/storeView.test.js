074c8b4be27fb2a1bc0a7dd9201ed99cf4b59f1b
Svetoch987 <clenet@mail.ru>
2017-07-14 11:57:27 +0300

    Update storeView.test.js

--- test/storeView.test.js
+++ test/storeView.test.js
@@ -2,17 +2,18 @@ describe('storeApp', function() {
 
   describe('storeController', function() {
 
-    /*beforeEach(function() {
-      browser.get('http://localhost:5050/#!/tienda', 5000);
+    beforeEach(function() {
+      browser.get('https://onlineshop.andersenlab.com/#!/tienda');
     });
 
     it('should check the items in shop', function() {
     /*  var itemList = element.all(by.repeater('product in query'));
 
       expect(itemList.count()).toBe(7);
-     expect(true).toBe(true);
+    */ expect(true).toBe(true);
+    
     });
 
-  });*/
+  });
 
 });
