71d9c7ad67347fcc23e69cbed61301a64322d6ae
Svetoch987 <clenet@mail.ru>
2017-07-17 10:41:22 +0300

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,10 +1,27 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'target/screenshots',
+  filename: 'my-report.html'
+});
+
 exports.config = {
+    beforeLaunch: function() {
+      return new Promise(function(resolve){
+        reporter.beforeLaunch(resolve);
+      });
+    },	
     specs: ['test/*test.js'],
     multiCapabilities: [
         {'browserName': 'chrome'}
     ],
-	onPrepare: function() {   
-	jasmine.getEnv().addReporter(        new Jasmine2HtmlReporter({          savePath: 'autotests/screenshots'      }));  
-	}
+	onPrepare: function() {
+    jasmine.getEnv().addReporter(reporter);
+  },
+	 afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
+  }
 }
